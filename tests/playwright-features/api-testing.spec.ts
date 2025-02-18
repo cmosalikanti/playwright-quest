f71a475ec344124e61c7fada808cda6fd1b161c7
Chandra Sekhar Mosalikanti <mcsekhar777@gmail.com>
2025-02-18 07:12:11 +0000

    use APIRequestContext for API Testing

--- tests/playwright-features/api-testing.spec.ts
+++ tests/playwright-features/api-testing.spec.ts
@@ -1,19 +1,27 @@
-import { test, expect, request } from '@playwright/test';
+import { test, expect, request, APIRequestContext } from '@playwright/test';
 
-test('GET api request', async({request}) => {
-    const results = await request.get('https://reqres.in/api/users/2');
+let apiContext: APIRequestContext;
+
+test.beforeAll(async() => {
+    apiContext = await request.newContext({
+        baseURL: 'https://reqres.in'
+    })
+});
+
+test('GET api request', async({}) => {
+    const results = await apiContext.get('/api/users/2');
     expect(results.status()).toBe(200);
     const responseBody = await results.json();
     expect(responseBody.first_name).not.toBeNull();
 });
 
-test('POST API request', async({request}) => {
+test('POST API request', async({}) => {
     const postData = {
         "name": "morpheus",
         "job": "leader"
     };
 
-    const response = await request.post('https://reqres.in/api/users', {
+    const response = await apiContext.post('/api/users', {
       data: postData,
     });
 
@@ -23,13 +31,13 @@ test('POST API request', async({request}) => {
     expect(responseBody.job).toBe(postData.job);
 });
 
-test('PUT API request', async({request}) => {
+test('PUT API request', async({}) => {
     const postData = {
         "name": "morpheus",
         "job": "zion resident"
     };
 
-    const response = await request.put('https://reqres.in/api/users/2', {
+    const response = await apiContext.put('/api/users/2', {
       data: postData,
     });
 
@@ -39,7 +47,7 @@ test('PUT API request', async({request}) => {
     expect(responseBody.job).toBe(postData.job);
 });
 
-test('DELETE API request', async({request}) => {
-    const response = await request.delete('https://reqres.in/api/users/2');
+test('DELETE API request', async({}) => {
+    const response = await apiContext.delete('/api/users/2');
     expect(response.status()).toBe(204);
 });
